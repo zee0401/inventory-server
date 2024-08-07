dec11aef4ef0135eeec2c661026d52fc15b8ce5f
zee0401 <41227625+zee0401@users.noreply.github.com>
2024-08-07 14:00:00 +0530

    routes

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 
 import dashboardRoutes from "./routes/dashboardRoutes";
+import productRoutes from "./routes/productRoutes";
 
 dotenv.config();
 const app = express();
@@ -20,6 +21,7 @@ app.use(cors());
 // Routes
 
 app.use("/dashboard", dashboardRoutes);
+app.use("/products", productRoutes);
 
 // Server
 const port = process.env.PORT || 3001;
