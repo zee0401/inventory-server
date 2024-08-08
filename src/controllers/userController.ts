be24a62c2a42cb85a065190fccadb7bca2bb2bdc
zee0401 <41227625+zee0401@users.noreply.github.com>
2024-08-08 12:41:39 +0530

    user controller and routes created

---
+++ src/controllers/userController.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from "express";
+
+const prisma = new PrismaClient();
+
+export const getUsers = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const users = await prisma.users.findMany();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({
+      message: "Error retrieving users",
+    });
+  }
+};
--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import morgan from "morgan";
 
 import dashboardRoutes from "./routes/dashboardRoutes";
 import productRoutes from "./routes/productRoutes";
+import userRoutes from "./routes/userRoutes";
 
 dotenv.config();
 const app = express();
@@ -22,6 +23,7 @@ app.use(cors());
 
 app.use("/dashboard", dashboardRoutes);
 app.use("/products", productRoutes);
+app.use("/users", userRoutes);
 
 // Server
 const port = process.env.PORT || 3001;
