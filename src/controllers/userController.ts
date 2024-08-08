0e0168d375a0deb08d1395e58d5f844db01ae971
zee0401 <41227625+zee0401@users.noreply.github.com>
2024-08-08 12:56:35 +0530

    bug fix

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,12 +1,11 @@
-import { PrismaClient } from "@prisma/client/extension";
+import { PrismaClient } from "@prisma/client";
 import { Request, Response } from "express";
 
 const prisma = new PrismaClient();
-
 export const getUsers = async (req: Request, res: Response): Promise<void> => {
   try {
     const users = await prisma.users.findMany();
-    res.json(users);
+    res.status(200).json(users);
   } catch (err) {
     res.status(500).json({
       message: "Error retrieving users",
